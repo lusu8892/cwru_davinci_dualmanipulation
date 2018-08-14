8445711500f9a2b75f367262ca3c4ad206ae36db
lusu8892 <sxl924@case.edu>
2018-08-14 00:16:43 -0400

    update

--- cwru_davinci_dual_manipulation_ik_control/src/davinci_group_structure_manager.cpp
+++ cwru_davinci_dual_manipulation_ik_control/src/davinci_group_structure_manager.cpp
@@ -62,8 +62,8 @@ void DavinciGroupStructureManager::parseParameters(XmlRpc::XmlRpcValue& params)
 
   bool correct(true);
 
-  parseSingleParameter(params,chain_names_list_,"chain_group_names",1);
-  parseSingleParameter(params,tree_names_list_,"tree_group_names",1);
+  cwru_davinci_dual_manipulation::shared::parseSingleParameter(params,chain_names_list_,"chain_group_names",1);
+  cwru_davinci_dual_manipulation::shared::parseSingleParameter(params,tree_names_list_,"tree_group_names",1);
 
   // list of chains composing each tree
   if(params.hasMember("tree_composition"))
@@ -97,8 +97,9 @@ void DavinciGroupStructureManager::parseParameters(XmlRpc::XmlRpcValue& params)
       tree_composition_.swap(tree_composition_tmp);
       tree_composition_tmp.clear();
     }
+  }
 
-    std::vector<std::string> tree_names_tmp;
+  std::vector <std::string> tree_names_tmp;
     tree_names_tmp.swap(tree_names_list_);
 
     for(auto tree:tree_names_tmp)
@@ -114,13 +115,55 @@ void DavinciGroupStructureManager::parseParameters(XmlRpc::XmlRpcValue& params)
       }
       else
         tree_names_list_.push_back(tree);
-
-      std::map<std::string, std::string> map_tmp, map_tmp_tree;
-      
-
     }
 
+  std::map <std::string, std::string> map_tmp, map_tmp_tree;
+  cwru_davinci_dual_manipulation::shared::parseSingleParameter(params, map_tmp, "group_map", chain_names_list_);
+  cwru_davinci_dual_manipulation::shared::parseSingleParameter(params, map_tmp_tree, "group_map", tree_names_list_);
 
+
+  if (!map_tmp_tree.empty())
+  {
+    for (auto tree : map_tmp_tree)
+    {
+      map_tmp[tree.first] = tree.second;
+    }
   }
 
+
+  if(!map_tmp.empty())
+  {
+    group_map_.swap(map_tmp);
+    map_tmp.clear();
+  }
+
+  if(group_map_.size() != chain_names_list_.size() + tree_names_list_.size())
+  {
+    std::cout << RED_INTENSE << CLASS_NAMESPACE << __func__ << " : group_map parameter has size " << group_map_.size()
+              << ", but it should be equal to the sum of chains and trees (" << chain_names_list_.size() << " + "
+              << tree_names_list_.size() << "): check the yaml configuration." << NO_COLOR << std::endl;
+    correct = false;
+  }
+  assert(correct);
+}
+
+bool DavinciGroupStructureManager::getGroupInSRDF(const std::string& ik_control_group, std::string& group_name) const
+{
+  if(group_map_.count(ik_control_group))
+  {
+    group_name = group_map_.at(ik_control_group);
+    return true;
+  }
+
+  return false;
+}
+
+const std::map< std::string, std::string >& DavinciGroupStructureManager::getGroupMap() const
+{
+  return group_map_;
+}
+
+const std::vector< std::string >& DavinciGroupStructureManager::getChains() const
+{
+  return chain_names_list_;
 }
